eecd6a1629fba8b7971cc352946c5ac7ac53785b
omkar chary <omkar@.com>
2026-01-24 22:46:03 +0530

    day 5,password generator

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,7 +1,7 @@
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
 
 bill = 0
 if size == "S":
@@ -18,5 +18,5 @@ if pepperoni == "Y":
     else:
             bill +=3
     if extra_cheese == "Y":
-                bill +=3
+                bill +=  1
     print(f"your final bil is : $ {bill}.")
---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,4 @@
+total = 0
+for number in range(1, 1021):
+    total =total+ number
+print(total)
