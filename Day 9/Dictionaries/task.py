06a15037e981311466a3ba903b352b2aa769ca2a
omkar chary <omkar@.com>
2026-05-15 11:45:11 +0530

    Blind auction project

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,23 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+'''programming_dictionary = {
+
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+print(programming_dictionary["Bug"] )
+'''
+colours = {
+    "apple": "red",
+    "pear": "green",
+    "banana": "yellow"
+}
+print(colours["pear"])
+my_empty_dictionary = {}
+print(my_empty_dictionary)
+colours["peach"]= "pink"
+print(colours)
+colours["apple"]= "green"
+print(colours)
+for key in colours:
+    print(key)
+for key in colours:
+    print(colours[key])
